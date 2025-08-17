6983723d110af33079080eaf133c330e37b75e22
JOSE-OMAR-FLORES <153056143+JOSE-OMAR-FLORES@users.noreply.github.com>
2025-08-16 18:32:23 -0600

    niu gg

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -76,7 +76,7 @@ class OrderController extends Controller
 
         $order = Order::create([
             'status' => 'pending',
-            'total_amount' => $total,
+            'total' => $total,
             'payment_method' => $request->payment_method,
             'customer_name' => $request->customer_name ?? null,
             'notes' => $request->notes ?? null,
